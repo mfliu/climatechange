1d40149bb3e89f48e24a8e42d15ec16ad64b95f8
Monica Liu <liu.monica.f@gmail.com>
2021-03-24 18:56:56 -0400

    Updates

--- cc.js
+++ cc.js
@@ -1,25 +1,25 @@
 const Groups = ["A", "B", "C"];
-const Actions = ["Convert to renewable energy",
-                 "Impose carbon taxes in your region",
-                 "Subsidize electric vehicles",
-                 "Subsidize public transit",
-                 "Subsidize improved insulation in housing to reduce heating and cooling energy",
-                 "Stop deforestation",
-                 "Start reforestation (must happen before 2060)",
-                 "Implement new agricultural technologies to decrease emissions",
-                 "Develop carbon sequestration technologies",
-                 "Develop waste reduction programs"];
+var Actions = { 1: "Convert to renewable energy",
+                2: "Impose carbon taxes in your region",
+                3: "Subsidize electric vehicles",
+                4: "Subsidize public transit",
+                5: "Subsidize improved insulation in housing to reduce heating and cooling energy",
+                6: "Stop deforestation",
+                7: "Start reforestation (must happen before 2060)",
+                8: "Implement new agricultural technologies to decrease emissions",
+                9: "Develop carbon sequestration technologies",
+                10: "Develop waste reduction programs"};
 
 const BonusActions = ["Contribute to international climate budget",
                       "Receive money from international climate budget"];
 
-const Costs_A = [-4, 4, -0.1, -0.5, -4, -0.1, -2, -4, -6, -2];
-const Costs_B = [-3, 6, -0.1, -0.5, -2, -2, -1, -3, -6, -2];
-const COSTS_C = [-1.5, 2, -0.1, -0.5, -2, -1, -0.25, -1.5, -6, -1];
+var Costs_A = {1:-4, 2:4, 3:-0.1, 4:-0.5, 5:-4, 6:-0.1, 7:-2, 8:-4, 9:-6, 10:-2};
+var Costs_B = {1:-3, 2:6, 3:-0.1, 4:-0.5, 5:-2, 6:-2, 7:-1, 8:-3, 9:-6, 10:-2};
+var Costs_C = {1:-1.5, 2:2, 3:-0.1, 4:-0.5, 5:-2, 6:-1, 7:-0.25, 8:-1.5, 9:-6, 10:-1};
 
-const Effects_A = [-0.1, -0.1, -0.1, -0.1, -0.2, -0.1, -0.1, -0.1, -0.2, -0.1];
-const Effects_B = [-0.2, -0.2, -0.1, -0.1, -0.1, -0.2, -0.3, -0.2, -0.2, -0.1];
-const Effects_C = [-0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.2, -0.1, -0.2, -0.1];
+var Effects_A = {1:-0.1, 2:-0.1, 3:-0.1, 4:-0.1, 5:-0.2, 6:-0.1, 7:-0.1, 8:-0.1, 9:-0.2, 10:-0.1};
+var Effects_B = {1:-0.2, 2:-0.2, 3:-0.1, 4:-0.1, 5:-0.1, 6:-0.2, 7:-0.3, 8:-0.2, 9:-0.2, 10:-0.1};
+var Effects_C = {1:-0.1, 2:-0.1, 3:-0.1, 4:-0.1, 6:-0.1, 6:-0.1, 7:-0.2, 8:-0.1, 9:-0.2, 10:-0.1};
 
 var Budget_A = 10;
 var Budget_B = 6;
@@ -41,9 +41,9 @@ var data = {
 };
 
 var groupActions = {
-  a: Actions,
-  b: Actions,
-  c: Actions
+  a: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  b: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  c: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 };
 
 function plotData() {
@@ -105,13 +105,13 @@ function makeActionPanel(input) {
     actionListInput.classList.add("form-check-input");
     actionListInput.setAttribute("type", "radio");
     actionListInput.setAttribute("name", "flexRadioDefault");
-    actionListInput.setAttribute("id", "action".concat(i.toString()));
+    actionListInput.setAttribute("id", "action".concat(countryActions[i].toString()));
     actionListDiv.appendChild(actionListInput);
 
     var actionListLabel = document.createElement("label");
     actionListLabel.classList.add("form-check-label");
-    actionListLabel.setAttribute("for", "action".concat(i.toString()));
-    actionListLabel.innerHTML = Actions[i];
+    actionListLabel.setAttribute("for", "action".concat(countryActions[i].toString()));
+    actionListLabel.innerHTML = Actions[countryActions[i]];
     actionListDiv.appendChild(actionListLabel);
 
     actionList.appendChild(actionListDiv);
@@ -122,18 +122,72 @@ function makeActionPanel(input) {
 
 function submitChoice(group) {
   var countryActions;
-  if (input === "A") {
+  var costs;
+  var effects;
+  if (group === "A") {
     countryActions = groupActions.a;
+    costs = Costs_A;
+    effects = Effects_A;
+    budget = Budget_A;
   }
-  else if (input == "B") {
+  else if (group == "B") {
     countryActions = groupActions.b;
+    costs = Costs_B;
+    effects = Effects_B;
+    budget = Budget_B;
   }
-  else if (input == "C") {
+  else if (group == "C") {
     countryActions = groupActions.c;
+    costs = Costs_C;
+    effects = Effects_C;
+    budget = Budget_C;
   }
-  for (var i=0; i<; i++) {
-    var actionListChild = actionListChildren[i];
-    console.log(actionListChild);
-    console.log(actionListChild.attributes);
+
+  for (var i=0; i<countryActions.length; i++) {
+    var form = document.getElementById("action".concat(countryActions[i].toString()));
+    if (form.checked == true) {
+      var actionCost = costs[countryActions[i]];
+      var actionEffect = effects[countryActions[i]];
+      budget = budget + actionCost;
+      TempChange = TempChange + actionEffect;
+      countryActions.splice(i);
+    }
   }
+  if (group === "A") {
+    groupActions.a = countryActions;
+    Budget_A = budget;
+  }
+  else if (group == "B") {
+    groupActions.b = countryActions;
+    Budget_B = budget;
+  }
+  else if (group == "C") {
+    groupActions.c = countryActions;
+    Budget_C = budget;
+  }
+
+  var contributeForm = document.getElementById("bonus".concat(group).concat("Give"));
+  var contribute = 0;
+  if (group === "A") {
+    Budget_A = Budget_A - contribute;
+  }
+  else if (group === "B") {
+    Budget_B = Budget_B - contribute;
+  }
+  else if (group === "C") {
+    Budget_C = Budget_C - contribute;
+  }
+  International = International + contribute;
+
+  var receive = document.getElementById("bonus".concat(group).concat("Get"));
+  if (group === "A") {
+    Budget_A = Budget_A + receive;
+  }
+  else if (group === "B") {
+    Budget_B = Budget_B + receive;
+  }
+  else if (group === "C") {
+    Budget_C = Budget_C + receive;
+  }
+  International = International - receive;
 };
